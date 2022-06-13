179c2ab7dd08b518ec8ece46c14a7724aa8c0963
madson <madsonmendes87@gmail.com>
2022-06-13 17:30:34 -0300

    primeiro commit

---
+++ pessoas.ts
@@ -0,0 +1,24 @@
+const pessoa1 = {
+    nome: 'Maria',
+    idade: 29,
+    profissao: 'atriz'
+};
+
+const pessoa2 = {
+    nome: 'Roberto',
+    idade: 19,
+    profissao: 'padeiro'
+};
+
+const pessoa3 = {
+    nome: 'Laura',
+    idade: '32',
+    profissao: 'atriz'
+};
+
+const pessoa4 = {
+    nome: 'Carlos',
+    idade: 19,
+    profissao: 'padeiro'
+};
+
